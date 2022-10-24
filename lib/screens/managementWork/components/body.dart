701759ca3db065648d8090a0bf3d75a9c555ef54
hungnv <hungnv@mor.com.vn>
2022-10-24 17:33:27 +0700

    update

--- lib/screens/managementWork/components/body.dart
+++ lib/screens/managementWork/components/body.dart
@@ -1,11 +1,64 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
   const Body({super.key});
 
   @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    String cdate = DateFormat("yyyy-MM-dd").format(DateTime.now());
+
+    return CardInfo();
+  }
+}
+
+class CardInfo extends StatelessWidget {
+  const CardInfo({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(5),
+      margin: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+      color: Colors.grey,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          RichText(
+            text: TextSpan(children: [
+              TextSpan(
+                text: 'Code du an moi\n',
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6
+                    ?.copyWith(fontWeight: FontWeight.bold),
+              ),
+              TextSpan(
+                  text: 'Lam gi do abc ABC',
+                  style: Theme.of(context).textTheme.bodyText2),
+            ]),
+          ),
+          const Spacer(),
+          Row(
+            children: const [
+              Text('10/03/2022 02 34'),
+              Icon(
+                Icons.watch_later_outlined,
+                size: 20,
+                color: Colors.red,
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/screens/managementWork/manager_work_screen.dart
+++ lib/screens/managementWork/manager_work_screen.dart
@@ -20,10 +20,52 @@ class Manager_Work extends StatelessWidget {
       ),
       body: Body(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          _showMaterialDialog(context);
+        },
         backgroundColor: Colors.green,
         child: const Icon(Icons.create),
       ),
     );
+  }
+
+  void _showMaterialDialog(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Material Dialog'),
+            content: SingleChildScrollView(
+              child: Column(children: [
+                 TextField(
+                  decoration:  InputDecoration(
+                      hintText: 'nhap cong viec', labelText: 'Cong Viec'),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: 'nhap cong viec', labelText: 'Mo Ta'),
+                ),
+              ]),
+            ),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () {
+                    _dismissDialog(context);
+                  },
+                  child: const Text('Close')),
+              TextButton(
+                onPressed: () {
+                  print('HelloWorld!');
+                  _dismissDialog(context);
+                },
+                child: const Text('HelloWorld!'),
+              )
+            ],
+          );
+        });
+  }
+
+  _dismissDialog(BuildContext context) {
+    Navigator.pop(context);
   }
 }
